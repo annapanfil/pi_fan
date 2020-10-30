21f8eb3eb2256c5045116eb4a705cb8c017ba58a
panka134 <134panka@gmail.com>
2020-10-30 17:02:54 +0100

    read rpm

--- fan_control.py
+++ fan_control.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import RPi.GPIO as gpio
 import os
@@ -11,14 +11,14 @@ class Resource:
 		self.on_temp = desired_temp + 2 # treshold temperature to turn on the fan
 		self.curr_temp = None
 		self.last_temp = None
-		self.fanStatus = 0 			# -1 – decrease fan speed, 1 – increase, 0 – maintain
+		self.fanStatus = 0  # what does resource want from fan: -1 – decrease speed, 1 – increase, 0 – maintain
 
 	def ordersForFan() -> int:
-		if self.status == 0:
-			if curr_temp > on_temp: self.fanStatus = 1
-		elif curr_temp < off_temp: self.fanStatus = 0
+		if self.fanstatus == 0:
+			if curr_temp > on_temp: self.fanStatus = 1	# turn on the fan
+		elif curr_temp < off_temp: self.fanStatus = 0	# "it's ok for me"
 		elif curr_temp < last_temp: self.fanStatus = -1	# temperature is decreasing – slow down the fan
-		return self.status
+		return self.fanstatus
 
 	def getTemp() -> int:
 		pass
@@ -29,16 +29,15 @@ class Disk(Resource):
 		self.name = name
 
 	def getTemp() -> int:
-		res = os.popen("sudo smartctl -d sat --all /dev/%s | grep Temperature_Celsius | awk '{print $10}'" % self.name).readline()
-		return int(res)	# cast result to int
+		result = os.popen("sudo smartctl -d sat --all /dev/%s | grep Temperature_Celsius | awk '{print $10}'" % self.name).readline()
+		return int(result)
 
 class CPU(Resource):
 	def getTemp() -> int:
 		f = open("/sys/class/thermal/thermal_zone0/temp")
 		CPUTemp = f.read()
 		f.close()
-		return int(CPUTemp.replace("\n",""))/1000	# remove return from result, cast to int and divide by 1000
-
+		return int(CPUTemp.replace("\n",""))/1000
 
 class Fan:
 	def __init__():
@@ -46,28 +45,27 @@ class Fan:
 		self.last_rpm = 0		# last fan pwm signal in %
 		self.rpm = 0.0			# rotational speed of fan in rpm
 		self.pwm_pin = None
+		self.t0 = time.time()	# timing variable to determine rpm
 
 	def calcrpm(channel):
 		# interrupt function that should calculate the rpm from tacho signal, but sometimes does not work due to interrupt
-		global t0			# get global variable
-		t1 = time.time()	# get current time
+		t1 = time.time()		# get current time
 		try:
-			#rpm = (60 / (t1-t0)) # This is the normal formula to calculate rpm
-			self.rpm = (30 / (t1-t0)) # Since there are two flanks per rotation we need to half it
-		except ZeroDivisionError:
-			pass
-		t0 = t1	# save time this was called
+			# self.rpm = (60 / (t1-self.t0)) # This is the normal formula to calculate rpm
+			self.rpm = (30 / (t1-self.t0)) # Since there are two flanks per rotation we need to half it
+		except ZeroDivisionError: pass
+		finally: self.t0 = t1
 
 	def testFan(killer):
 		fan.pwm_pin.ChangeDutyCycle(100)	# set fan to fully on
-		time.sleep(1)				# wait for fan to stabilize
-		if(not rpm > 1000):			# check if fan is blocked -> at least that is what happened in my case
+		time.sleep(1)						# wait for fan to stabilize
+		if(not self.rpm > 1000):			# check if fan is blocked -> at least that is what happened in my case
 			os.system('echo \'Fancontrol: Warning fan might be blocked!\' | wall')	# announce error
 			killer.thread_dont_terminate = False	# terminate script
-		elif(not rpm):	# check if fan is broken
+		elif(not self.rpm):							# check if fan is broken
 			os.system('echo \'Fancontrol: Warning fan might be broken!\' | wall')	# announce error
 			killer.thread_dont_terminate = False	# terminate script
-		fan.pwm_pin.ChangeDutyCycle(0)		# turn fan off
+		fan.pwm_pin.ChangeDutyCycle(0)				# turn fan off
 
 	def increase_rpm():
 		if self.rpm < 30: self.desired_rpm = 30
@@ -76,8 +74,8 @@ class Fan:
 	def decrease_rpm():
 		if self.rpm > 30: self.desired_rpm -= 5
 
-		# class helping with killing signals so gpio's can be cleaned up
 class GracefulKiller:
+	# class helping with killing signals so gpio's can be cleaned up
 	thread_dont_terminate = True
 	def __init__(self):
 		signal.signal(signal.SIGINT, self.exit_gracefully)
@@ -89,41 +87,40 @@ class GracefulKiller:
 # Configuration
 FAN_PWM_GPIO = 26			# gpio pin the fan is connected to
 FAN_SPEED_GPIO	= 19		# gpio pin the fan tachometer is connected to
-PWM_FREQUENCY = 20		# frequency of the pwm signal to control the fan with
+PWM_FREQUENCY = 20			# frequency of the pwm signal to control the fan with
 resources = (CPU(40), Disk(30, "sda"))
 fan = Fan()
 
-t0 = time.time()	# timing variable to determine rpm
-
-gpio.setmode(gpio.BCM)			 # Select pin reference
-gpio.setup(FAN_SPEED_GPIO, gpio.IN, pull_up_down = gpio.PUD_UP) # Declare fan speed pin as input and activate internal pullup resistor
-gpio.setup(FAN_PWM_GPIO, gpio.OUT) # Declare pwm fan pin as output
-fan.pwm_pin = gpio.PWM(FAN_PWM_GPIO, PWM_FREQUENCY) # Setup pwm on fan pin
-fan.pwm_pin.start(0) # Setup fan pwm to start with 0% duty cycle
-
-
 # Add aboth function as interrupt to tacho pin on rising edges (can also be falling does not matter)
-gpio.add_event_detect(FAN_SPEED_GPIO, gpio.RISING, callback = fan.calcrpm)
+gpio.add_event_detect(FAN_SPEED_GPIO, gpio.RISING, callback = fan.calcrpm) # bouncetime=200 (in ms) to avoid "switch bounce"
 
 
 if __name__ == '__main__':
+	gpio.setmode(gpio.BCM)				 # Select pin reference
+	gpio.setup(FAN_SPEED_GPIO, gpio.IN, pull_up_down = gpio.PUD_UP) # Declare fan speed pin as input and activate internal pullup resistor
+	gpio.setup(FAN_PWM_GPIO, gpio.OUT)	 # Declare pwm fan pin as output
+	fan.pwm_pin = gpio.PWM(FAN_PWM_GPIO, PWM_FREQUENCY) # Setup pwm on fan pin
+	fan.pwm_pin.start(0) 				 # Setup fan pwm to start with 0% duty cycle
+
 	killer = GracefulKiller()
-	fan.testFan(killer)				# test the fan
-	while killer.thread_dont_terminate:	# main loop
+	fan.testFan(killer)		# test the fan
+
+	# main loop
+	while killer.thread_dont_terminate:
 		if(fan.rpm == 0 and fan.desired_rpm > 0):	# check if fan is dead
-			os.system('echo \'Fancontrol: Warning fan might be broken!\' | wall')	# announce error
+			os.system('echo \'Fancontrol: Warning fan might be broken!\' | wall')
 			killer.thread_dont_terminate = False	# stop everything
-			fan.pwm_pin.ChangeDutyCycle(0)	# turn fan off
-			break	# stop loop
+			fan.pwm_pin.ChangeDutyCycle(0)			# turn fan off
+			break
 
 		fanOrders = set()
-		for r in resources:
-			r.getTemp()		# get cpu and disk temperature
-			fanOrders.add(r.ordersForFan())
+		for resource in resources:
+			resource.getTemp()		# get cpu and disk temperature
+			fanOrders.add(resource.ordersForFan())
 
-		if 1 in fanOrders: fan.increase_rpm()
-		elif -1 in fanOrders: fan.decrease_rpm()
-		else: fan.desired_rpm = 0
+		if 1 in fanOrders: fan.increase_rpm()	 # if anyone wants to increase speed
+		elif -1 in fanOrders: fan.decrease_rpm() # if anyone wants to decrease speed
+		else: fan.desired_rpm = 0				 # if everyone feels ok – turn off the fan
 
 		if(fan.desired_rpm != fan.last_fan):	# change duty cycle if changed
 			fan.last_fan = fan.desired_rpm
---
+++ read_rpm.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import RPi.GPIO as gpio
+import time
+
+t0 = time.time()
+FAN_SPEED_GPIO	= 19
+gpio.setmode(gpio.BCM)				 # Select pin reference
+gpio.setup(FAN_SPEED_GPIO, gpio.IN, pull_up_down = gpio.PUD_UP) # Declare fan speed pin as input and activate internal pullup resistor
+
+def calcrpm(channel):
+    # interrupt function that should calculate the rpm from tacho signal, but sometimes does not work due to interrupt
+    global t0
+    t1 = time.time()		# get current time
+    try:
+        #rpm = (60 / (t1-t0)) # This is the normal formula to calculate rpm
+        rpm = (30 / (t1-t0)) # Since there are two flanks per rotation we need to half it
+    except ZeroDivisionError: pass
+    finally: t0 = t1
+    print(rpm)
+
+gpio.add_event_detect(FAN_SPEED_GPIO, gpio.RISING, callback = fan.calcrpm)
